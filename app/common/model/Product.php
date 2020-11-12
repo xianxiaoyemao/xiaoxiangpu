59e414ab467025b5b06c1929afee29d6cccc285e
92134310@qq.com <liangchi4310@sina.com>
2020-11-12 18:14:39 +0800

    .env

--- app/common/model/Product.php
+++ app/common/model/Product.php
@@ -16,7 +16,7 @@ class Product extends BaseModel
     protected $createTime = 'createtime';
     protected $updateTime = 'updatetime';
 
-    public function prodeuctSku ()
+    public function skus ()
     {
         return $this->hasMany(ProductSku::class);
     }
@@ -73,43 +73,24 @@ class Product extends BaseModel
 //        'status' => 'boolean', // on_sale 是一个布尔类型的字段
 //    ];
 
-//    public function toESArray(){
-//        // 只取出需要的字段
-//        $arr = Arr::only($this->toArray(), [
-//            'id',
-//            'product_core',
-//            'title',
-//            'long_title',
-//            'bar_code',
-//            'status',
-//            'audit_status',
-//            'rating',
-//            'sold_count',
-//            'review_count',
-//            'price',
-//            'image'
-//        ]);
-//
-//        // 如果商品有类目，则 category 字段为类目名数组，否则为空字符串
-//        $arr['category'] = $this->category ? explode(' - ', $this->category->full_name) : '';
-//        // 类目的 path 字段
-//        $arr['category_path'] = $this->category ? $this->category->path : '';
-//        // strip_tags 函数可以将 html 标签去除
-//        $arr['description'] = strip_tags($this->productdescriptions["description"]);
-//        // 只取出需要的 SKU 字段
-//        $arr['skus'] = $this->skus->map(function (ProductSku $sku) {
-//            return Arr::only($sku->toArray(), ['title', 'description', 'price']);
-//        });
-//        $arr['shop_name'] = $this->shop->name;
-//        // 只取出需要的商品属性字段
-//        $arr['properties'] = $this->properties->map(function (ProductProperty $property) {
-//            return Arr::only($property->toArray(), ['name', 'value']);
-//        });
-//
-//        $arr['images'] = $this->images->map(function (ProductImage $productimage){
-//            return Arr::only($productimage->toArray(),['image_url']);
-//        });
-//
-//        return $arr;
-//    }
+    public function toEsarray(){
+        // 只取出需要的字段
+        $arr = $this -> visible([
+            'id',
+            'title',
+            'bar_code',
+            'images',
+            'price',
+            'sales',
+            'introduce',
+            'rating',
+            'review',
+            'status',
+            'category_id',
+            'shop_id'
+        ]) -> toArray();
+        $arr['category'] = Category::find($arr['category_id'])['cate_name'];
+        $arr['shop_name'] = Shops::find($arr['shop_id'])['title'];
+        return $arr;
+    }
 }
