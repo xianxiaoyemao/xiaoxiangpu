5b117969ffc1e487b5871042c5ef78c6602e7278
Administrator <liuqianglan@sina.com>
2020-11-24 15:54:06 +0800

    积分兑换

--- app/api/controller/Personal.php
+++ app/api/controller/Personal.php
@@ -3,6 +3,7 @@
 namespace app\api\controller;
 
 use app\BaseController;
+use app\common\model\Config;
 use app\common\model\User;
 use app\Request;
 use think\App;
@@ -10,10 +11,14 @@ use think\App;
 class Personal extends BaseController
 {
     protected $userModel = null;
+
+    protected $convert = 0;
+
     public function __construct(App $app)
     {
         parent::__construct($app);
         $this->userModel = new User();
+        $this->convert = Config::where('name', 'scoreConvert')->value('value');
     }
 
     /**
@@ -29,6 +34,30 @@ class Personal extends BaseController
         $uid = $request->post('uid');
         $row = $this->userModel->where('id', $uid)->field('nickname, avatar, money, score')->find()->toArray();
         if (!$row) return apiBack('fail', '暂无数据', '10004');
-        return apiBack('success', '成功', '10000', $row);
+        $data = [
+          'row' => $row,
+          'convertValue' => $this->convert
+        ];
+        return apiBack('success', '成功', '10000', $data);
+    }
+
+    /**
+     * 兑换现金
+     * @param Request $request
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function convert (Request $request)
+    {
+        if (!$request->isPost()) return apiBack('fail', '请求方式错误', '10004');
+        $score = $request->post('score');
+        $uid = $request->post('uid');
+        $user = User::where('id', $uid)->find();
+        $user->money += intval($score / $this->convert);
+        $user->score = $score % $this->convert;
+        $user->save();
+        return apiBack('success', '兑换成功', '10000');
     }
 }
