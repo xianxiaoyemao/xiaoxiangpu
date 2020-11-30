19abbd5674275e8f7d566e5043e675e9162189ef
Administrator <liuqianglan@sina.com>
2020-11-30 11:59:49 +0800

    命名修改

--- app/api/controller/Cartitem.php
+++ app/api/controller/Cartitem.php
@@ -295,7 +295,7 @@ class Cartitem extends BaseController{
                 $detail[$key]['number'] = $val['quantity'];
                 $detail[$key]['specvalue'] = $val['specvalue'];
                 $detail[$key]['speckey'] = $val['speckey'];
-                $detail[$key]['createtime'] = $val['createtime'];
+                $detail[$key]['createtime'] = time();
 
                 $product_price += $val['price'] * $val['quantity'];
             }
