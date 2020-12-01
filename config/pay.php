35e3b578bac595f1c102976fea4ecc3188a6bbeb
Administrator <liuqianglan@sina.com>
2020-12-01 11:38:07 +0800

    命名修改

--- config/pay.php
+++ config/pay.php
@@ -8,14 +8,12 @@ return [
             'file' => app()->getRuntimePath().'public/storage/paylogs/alipay.log',
         ],
     ],
-    'wechat' => [
-        'app_id' => '',
-        'mch_id' => '',
-        'key' => '',
-        'cert_client' => '',
-        'cert_key' => '',
+    'miniprogram' => [
+        'app_id' => 'wx2321f85bc3478c47',
+        'mch_id' => '1604563313',
+        'key' => 'xianxiaoyemao1234567890XIAOYEMAO',
         'log' => [
-            'file' => app()->getRuntimePath().'public/storage/paylogs/wechat_pay.log',
+            'file' => app()->getRuntimePath().'public/storage/paylogs/mini_pay.log',
         ],
     ],
 ];
